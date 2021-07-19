814ca68fe78790a8761a072a8d4ccacb35fd58a6
TheRealPaulyglot <paulsanchezco@gmail.com>
2021-07-19 16:30:29 -0400

    Update Break camelCase.js

--- Javascript/Break camelCase.js
+++ Javascript/Break camelCase.js
@@ -1,4 +1,11 @@
-// complete the function
-function solution(string) {}
+function solution(string) {
+  let words = "";
+  for (var key in string)
+    words +=
+      string[key] == string[key].toUpperCase()
+        ? " " + string[key]
+        : string[key];
 
-solution(camelCasing);
+  return words.trim();
+}
+console.log(solution("CamelCasingTest"));
