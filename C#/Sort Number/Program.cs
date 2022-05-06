3d60b77de16888b9046dae97014886efa597deb5
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-06 16:26:59 -0400

    Gone to work!

---
+++ C#/Sort Number/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int[] arr = new[] { 1, 10, 2, 50,5 };
+Kata.SortNumbers(arr).ToList().ForEach(number => Console.WriteLine(number));
+
+public static class Kata
+{
+    public static int[] SortNumbers(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums;
+    }
+}
