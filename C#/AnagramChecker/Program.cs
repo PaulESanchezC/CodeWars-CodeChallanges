d48ef73a45e231cebc10e50a2fdbb5fa3e340701
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-06 08:30:55 -0400

    gotta get back to this. apparently this is what can determine if i can code for a real life project :S

---
+++ C#/AnagramChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿AnagramTools.AnagramCheck("anagram", "gramana");
+
+public class AnagramTools
+{
+    public static void AnagramCheck(string input, string output)
+    {
+        var inputCharacters = input.ToCharArray().ToList();
+        var checker = new Dictionary<char, int>();
+
+        var i = 0;
+        inputCharacters.ForEach(letter =>
+            {
+                if (output.Contains(letter))
+                    checker.TryAdd(letter, i++);
+            });
+
+        Console.WriteLine(checker.Keys.Count == output.Distinct().Count());
+    }
+}
---
+++ C#/Highest Scoring Word/Program.cs
@@ -0,0 +1,21 @@
+﻿var response = HighestScoringWord.GetHighestScoringWord("man i need a taxi up to ubud");
+Console.WriteLine(response);
+
+public class HighestScoringWord
+{
+    public static string GetHighestScoringWord(string sentence)
+    {
+        List<string> wordList = new();
+        List<(int key,string value)> mvpList = new();
+
+        wordList.AddRange(sentence.Split().ToList());
+        wordList.ForEach(word =>
+        {
+            mvpList.Add((word.ToCharArray().Length,word));
+        });
+
+        var mvpIndex = mvpList.Max().key;
+
+        return mvpList[mvpIndex].value;
+    }
+}
