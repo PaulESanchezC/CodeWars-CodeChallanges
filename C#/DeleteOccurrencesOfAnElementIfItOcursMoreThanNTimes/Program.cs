be212712782b3e53ea92a9cfa292a2b7ff71da09
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-10 01:58:45 -0400

    Fun one

---
+++ C#/DeleteOccurrencesOfAnElementIfItOcursMoreThanNTimes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+public class Kata
+{
+    public static int[] DeleteNth(int[] arr, int x)
+    {
+        Dictionary<int, int> dictionary = new();
+        arr.ToList().ForEach(item =>
+       {
+           if (!dictionary.ContainsKey(item))
+               dictionary.Add(item, 1);
+           dictionary[item] += dictionary[item] >= x ? 0 : 1;
+       });
+        return dictionary.Keys.ToArray();
+    }
+}
