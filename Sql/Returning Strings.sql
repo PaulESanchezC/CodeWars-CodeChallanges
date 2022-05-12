f3160b64083efc22b60c99679fe40bc8ced66c83
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-11 23:23:01 -0400

    Update

--- Sql/Returning Strings.sql
+++ Sql/Returning Strings.sql
@@ -1,2 +1,5 @@
 
+--Write a select statement that takes name from person table and return
+--"Hello, <name> how are you doing today?" results in a column named greeting
+--https://www.codewars.com/kata/55a70521798b14d4750000a4/train/sql
 Select CONCAT('Hello, ',name ,' how are you doing today?') as greeting from person;
