f55c7525bf6acec63ee9f1ef37e9ec4a2d47a901
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-17 11:39:49 -0400

    k thats it!
    time to work!

---
+++ C#/Convert a Number to a String!/ConvertNumberToString.cs
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/csharp
+// need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num) => $"{num}"; 
+}
+
+
