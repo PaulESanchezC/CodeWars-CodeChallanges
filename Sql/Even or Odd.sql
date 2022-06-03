e947aa1274769e6f66d2325f540909cee56320c2
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-06-03 13:22:28 -0400

    warm ups after vacay

---
+++ Sql/Even or Odd.sql
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/sql
+
+You will be given a table, numbers, with one column number.
+Return a table with a column is_even containing "Even" or "Odd" depending on number column values.
+
+numbers table schema
+number INT
+output table schema
+is_even STRING
+*/
+SELECT 
+  CASE
+    WHEN number % 2 = 0 THEN 'Even'
+    ELSE 'Odd'
+  END
+AS is_even
+FROM numbers;
